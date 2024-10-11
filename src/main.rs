307bba619c1973f86651e6cbd8e17e58dc2c9087
Rhenan Silverio Teixeira <rhenancontato@gmail.com>
2024-10-11 17:24:47 -0300

    create animation for title

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,49 @@
 extern crate rand;
 extern crate colored;
 
-use std::io;
+use std::{io::{self, Write}, thread::sleep, time::Duration};
 use colored::Colorize;
 use std::cmp::Ordering;
 use rand::Rng;
 
+fn print_for_animation(text: &str) {
+    for c in text.chars() {
+        print!("{}", c.to_string().red());
+        io::stdout().flush().unwrap();
+        sleep(Duration::from_millis(100));
+    }
+    println!();
+}
 fn main() {
-    println!("{}", "Jogo da adivinhação".red());
+    print_for_animation("Bem vindo ao jogo da adivinhação");
+
+    println!("Escolha um nível de dificuldade");
+    println!("{}","1- Fácil".blue());
+    println!("{}", "2- Médio".purple());
+    println!("{}", "3- Difícil".red());
+
+    let mut modo = String::new();
+    io::stdin().read_line(&mut modo).expect("Falha ao ler a entrada");
+    let modo: u32 = modo.trim().parse().expect("Por favor, insira um número");
+
+    let (vidas_iniciais, dicas_detalhadas) = match modo {
+        1 => (15, true),
+        2 => (10, false),
+        3 => (5, false),
+        _ => {
+            println!("Modo inválido! Iniciando no modo médio por padrão.");
+            (10, false) 
+        }
+    };
 
    loop {
    let mut fase = 1;
-   let mut points = 10;
+   let mut points = vidas_iniciais;
+
 
     loop {
-        let limite = fase * 100;
-    
+       
+    let limite = fase * 100;
     println!("Fase {}: Adivinhe um número entre 1 e {}", fase, limite);
     println!("Você tem {} chances", points);    
     
@@ -42,18 +70,33 @@ fn main() {
         match palpite.cmp(&numero_secreto) {
             Ordering::Less => {
                 points -= 1;
-                println!("Muito baixo! Perdeu ponto");  
-                if palpite + 10 >= numero_secreto {
-                    println!("Dica: Você está próximo");
+                
+                if  dicas_detalhadas {
+                    if (numero_secreto - palpite) > 50 {
+                        println!("{}", "Muito baixo! Tente um número bem maior.".red());
+                    } else if (numero_secreto - palpite) > 20 {
+                        println!("{}", "Um pouco baixo! Tente um número maior.".red());
+                    } else {
+                        println!("{}", "Quase! Um pouco maior.".red());
+                    }  
+                } else {
+                    println!("{}", "Muito baixo! Perdeu uma vida".red());  
                 }
+                
             } 
             Ordering::Greater => {
-                
                 points -= 1;
-                println!("Muito alto! Perdeu ponto");
-
-                if palpite - 10 <= numero_secreto {
-                    println!("Dica: Você está quase lá");
+                
+                if  dicas_detalhadas {
+                    if (numero_secreto - palpite) > 50 {
+                        println!("{}", "Muito baixo! Tente um número bem menor.".red());
+                    } else if (numero_secreto - palpite) > 20 {
+                        println!("{}", "Um pouco baixo! Tente um número menor.".red());
+                    } else {
+                        println!("{}", "Quase! Um pouco menor.".red());
+                    }  
+                } else {
+                    println!("{}", "Muito baixo! Perdeu uma vida".red());  
                 }
             } 
             Ordering::Equal => {
